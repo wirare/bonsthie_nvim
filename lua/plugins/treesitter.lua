faa0cfdd52ddfd61ad7c5a361aa433c6a194c506
Eliot Langlois <eliot.langlois@neuralcam.com>
2026-02-17 14:06:46 +0100

    Fix treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,23 +1,28 @@
 return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = ':TSUpdate', -- Use `build` instead of `run` for Lazy.nvim
-        config = function()
-            require('nvim-treesitter.configs').setup {
-                ensure_installed = { "lua", "c", "markdown" },
-				auto_install = true,
-                highlight = {
-                    enable = true,
-                    use_languagetree = true,
-                },
-                indent = { enable = true },
-            }
-        end,
-    },
-    {
-        "nvim-treesitter/playground",
-        cmd = "TSPlaygroundToggle",
-        config = function()
-        end,
-    }
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			require("nvim-treesitter").setup({
+				install_dir = vim.fn.stdpath("data") .. "/site",
+			})
+
+			require("nvim-treesitter").install({ "lua", "c", "markdown" })
+
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = { "lua", "c", "markdown" },
+				callback = function()
+					vim.treesitter.start()
+				end,
+			})
+
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = { "lua", "c" },
+				callback = function()
+					vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+				end,
+			})
+		end,
+	}
+
 }
